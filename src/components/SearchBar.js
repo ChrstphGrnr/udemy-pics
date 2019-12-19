80f42a9ec77b6ad976482685b30b21294b686a43
chrstphgrnr <christophgruener@me.com>
2019-12-18 17:51:10 -0700

    change onFormSubmit to allow "this"

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -10,18 +10,24 @@ class SearchBar extends React.Component {
     //     this.setState({searchTerm: e.target.value})
     // }
 
+    onFormSubmit = (e) => {
+        e.preventDefault();
+        
+        console.log(this.state.searchTerm)
+    }
+
     render() {
         return (
         <div>
             <div className="ui segment">
                 <h1>Search for Images</h1>
-                    <form className="ui form">
+                    <form className="ui form" onSubmit={this.onFormSubmit}>
                         <div className="field">
                             <label>Image Search</label>
                             <input
                                  type="text" 
                                 value={this.state.searchTerm} 
-                                onChange={e => this.setState({searchTerm: e.target.value})} 
+                                onChange={e => this.setState({searchTerm: e.target.value })} 
                             />
                         </div>
                         </form>
