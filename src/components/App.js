8e5ca216b58b0d3e42ed9133c1e5a712c257d180
chrstphgrnr <christophgruener@me.com>
2019-12-20 16:36:40 -0700

    refactor API call to unsplash with axios

--- src/components/App.js
+++ src/components/App.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import axios from 'axios'
+import unsplash from '../api/unsplash'
 
 import SearchBar from './SearchBar'
 
@@ -7,21 +7,22 @@ import SearchBar from './SearchBar'
 
 class App extends React.Component {
 
-    onSearchSubmit = (term) => {
-        axios.get('https://api.unsplash.com/search/photos', {
-            params: { query: term},
-            headers: {
-                Authorization: 'Client-ID 39d972e6632d1f3a8afb46962657c7427fcb09cf7b75a5635d260534ac7ca53d'
-            }
-        })
-        .then(resp => console.log(resp.results))
-        
+    state = {
+        images: [ ]
+    }
+
+    onSearchSubmit = async (term) => {
+        const response = unsplash.get('/search/photos', {
+                params: { query: term}   
+            });     
+        this.setState({images: response.data.results})
     }
     
     render() {
     return (
         <div className="ui container" style={{marginTop: '10px'}}>
             <SearchBar onSearchSubmit={this.onSearchSubmit}/>
+            Found: {this.state.images.length} images!
         </div>
     )}
 }
