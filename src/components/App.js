74db19821fe0c60e41f0f83a4a7f72936fd4eb96
chrstphgrnr <christophgruener@me.com>
2019-12-19 19:10:13 -0700

    add axios call and refactor onSearchSubmit function

--- src/components/App.js
+++ src/components/App.js
@@ -1,17 +1,27 @@
 import React from 'react'
+import axios from 'axios'
+
 import SearchBar from './SearchBar'
+
 
 
 class App extends React.Component {
 
-    searchTerm = (props) => {
-        console.log(props)
+    onSearchSubmit = (term) => {
+        axios.get('https://api.unsplash.com/search/photos', {
+            params: { query: term},
+            headers: {
+                Authorization: 'Client-ID 39d972e6632d1f3a8afb46962657c7427fcb09cf7b75a5635d260534ac7ca53d'
+            }
+        })
+        .then(resp => console.log(resp.results))
+        
     }
     
     render() {
     return (
         <div className="ui container" style={{marginTop: '10px'}}>
-            <SearchBar searchTerm={this.searchTerm}/>
+            <SearchBar onSearchSubmit={this.onSearchSubmit}/>
         </div>
     )}
 }
