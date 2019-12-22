03165f0348cc8910c9ec5684c445127e0ae7899b
chrstphgrnr <christophgruener@me.com>
2019-12-22 15:48:30 -0700

    allow 25 images to be returned from Unsplash API

--- src/components/App.js
+++ src/components/App.js
@@ -14,7 +14,7 @@ class App extends React.Component {
 
     onSearchSubmit = async (term) => {
         const response = await unsplash.get('/search/photos', {
-                params: { query: term}   
+                params: { query: term, per_page: 25}   
             });     
         this.setState({images: response.data.results})
         // console.log(this.state.images)
